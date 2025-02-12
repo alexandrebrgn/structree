2d770e157af64cff3307163478dd1563d5c7de2e
bourgu20al <bourgu20al@lycee-ndduroc.com>
2025-02-12 15:35:10 +0100

    feat: add context.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import {useContext} from "react";
+import {MyStructreeContext} from "./context/context.tsx";
 
 function App() {
 
+  const[rawTree, setRawTree] = useContext(MyStructreeContext)
+
   return (
     <div className="h-screen w-screen flex items-center">
-
+      <div className="mx-auto"></div>
     </div>
   )
 }
