8c02055ab5c55d1861a2145ebaf230608e947bb4
bourgu20al <bourgu20al@lycee-ndduroc.com>
2025-02-13 11:08:01 +0100

    feat: add copy-to-clipboard button

--- src/components/OutputTextArea.tsx
+++ src/components/OutputTextArea.tsx
@@ -1,7 +1,18 @@
 import useStructree from "../hooks/hooks.tsx";
+import {Copy} from "lucide-react";
 
 export default function OutputTextArea() {
   const {finalTree} = useStructree()
+
+  function CopyButton() {
+    return (
+      <div className="flex items-center justify-center rounded-md h-8 w-8 absolute top-2 right-2 hover:bg-gray-200">
+        <button className="hover:cursor-pointer" title="Copy to clipboard" onClick={() => {
+          navigator.clipboard.writeText(finalTree).then()
+        }}><Copy className="h-4"/></button>
+      </div>
+    )
+  }
 
   return (
     <div className="relative">
@@ -12,6 +23,7 @@ export default function OutputTextArea() {
         disabled
         style={{whiteSpace: 'pre'}}
       />
+      <CopyButton/>
     </div>
   )
 }
